946bf06d6fc39831a04272546b3857879ecc3c59
“alerenna” <alessandrorennafm@gmail.com>
2025-02-28 19:38:20 +0800

    Add displayer function and displayed cards in DOM

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
 // DOM const
 const blogEl = document.querySelector('.blog')
 
-//axios call for find the objects
-axios.get('https://lanciweb.github.io/demo/api/pictures/')
-    .then(res => {
-        console.log(res.data);
-
-        pictureInfo.forEach(thisPic => {
-            console.log(thisPic);
-    
-            const titlePic = thisPic.title
-            console.log(titlePic);
-            const datePic = thisPic.date
-            console.log(datePic);
-            const urlPic = thisPic.url
-            console.log(urlPic);
-            
-    
-            markupCard()     
-            
-        });
-
-        blogEl.appendChild()
-
-
-
-
-
-    })
-    .catch(error => { console.log(error); })
-
-
-// create a markup function
-function markupCard(titlePic, datePic, urlPic) {
+//Functions
+// create a markup generator function
+function markupCardGenerator(pic) {
 
     const markup =
     `<div class="col-4">
         <div class="card-custom bg-body m-5 position-relative" style="width:18rem;">
             <img class="pin position-absolute top-0 start-50 translate-middle" src="assets/img/pin.svg" style="width:2rem;" alt="">
 
-            <img src=${urlPic} class="card-img-top p-3" alt="...">
+            <img src=${pic.url} class="card-img-top p-3" alt="...">
             <div class="card-body">
-                <h6 class="card-date mb-2 text-muted ps-3">${datePic}</h6>
-                <h5 class="card-title ps-3 pb-3">${titlePic}</h5>
+                <h6 class="card-date mb-2 text-muted ps-3">${pic.date}</h6>
+                <h5 class="card-title ps-3 pb-3">${pic.title}</h5>
 
             </div>
         </div>
     </div>`
 
+    return markup
+
 }
 
-//create a function for generate the cards
-function cardGenerator(){
+//create a function insert the card in the DOM
+function cardDisplayer(res){
+    let pictureList = res.data
 
+        pictureList.forEach(pic => {
+            console.log(pic);
+
+            const markup = markupCardGenerator(pic)
+
+            blogEl.insertAdjacentHTML('beforeend', markup)
+            
+        }); 
   
 }
+
+
+//axios call for find the objects and call inside the display fucntion
+axios.get('https://lanciweb.github.io/demo/api/pictures/')
+    .then(res => {
+        console.log(res.data);
+
+        cardDisplayer(res)
+
+    })
+    .catch(error => { console.log(error); })
+
+
+
 
 
 
