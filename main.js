18e03f770c8bbb56a3374024cedc1dd658999ad7
“alerenna” <alessandrorennafm@gmail.com>
2025-03-03 17:50:45 +0800

    Edit eventlistener. Now Eventlistener for pictures click is on function cardDisplayer

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const blogEl = document.querySelector('.blog')
 const overlayEl = document.querySelector('.overlay-container')
 const closeButtonEl = document.querySelector('.close')
 
+
 //Functions
 // create a markup generator function
 function markupCardGenerator(pic) {
@@ -12,7 +13,7 @@ function markupCardGenerator(pic) {
         <div class="card-custom bg-body mt-4 position-relative" >
             <img class="pin position-absolute top-0 start-50 translate-middle" src="./assets/img/pin.svg" style="width:2rem;" alt="">
 
-            <img id="image" src=${pic.url} class="card-img-top p-3" alt="...">
+            <img src=${pic.url} class="card-img-top p-3" alt="...">
             <div class="card-body">
                 <h6 class="card-date mb-2 text-muted ps-3">${pic.date}</h6>
                 <h5 class="card-title ps-3 pb-3">${pic.title}</h5>
@@ -37,7 +38,22 @@ function cardDisplayer(res){
             blogEl.insertAdjacentHTML('beforeend', markup)
             
         }); 
-  
+
+    const imgCardEl = document.querySelectorAll('.card-img-top')                
+    console.log(imgCardEl);
+
+    for (let i = 0; i < imgCardEl.length; i++) {
+        const thisImage = imgCardEl[i];
+
+        thisImage.addEventListener('click', function() {
+
+            overlayEl.classList.remove('d-none')
+        
+            console.log('CLICK');
+        })
+        
+    }
+        
 }
 
 
@@ -52,17 +68,6 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
     .catch(error => { console.log(error); })
 
 
-const imgCardEl = document.querySelectorAll('#image')
-
-//add event listener on click on images to open overlay (remove d-none class from overlay div)
-imgCardEl.addEventListener('click', function() {
-
-    overlayEl.classList.remove('d-none')
-
-    console.log('CLICK');
-})
-
-
 //add event listener on click on button close to close overlay (add d-none class to overlay div)
 closeButtonEl.addEventListener('click', function() {
 
@@ -70,6 +75,13 @@ closeButtonEl.addEventListener('click', function() {
 
     console.log('CLICK CLOSE');
 })
+
+
+//Quando l'immagine viene cliccata appare la stessa immagine in overlay
+
+//Recupero l'immagine corrispondente con 
+
+//
 
 
 
